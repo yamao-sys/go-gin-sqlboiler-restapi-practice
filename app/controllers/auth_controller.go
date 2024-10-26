3a4a004aa0f0ee77bc1e8c38d63ac5f1eb2992d6
yamao-sys <yamaopanku@gmail.com>
2024-10-26 15:10:29 +0900

    認証関連の実装

---
+++ app/controllers/auth_controller.go
@@ -0,0 +1,83 @@
+package controllers
+
+import (
+	"app/dto"
+	"app/services"
+	"app/utils"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type AuthController interface {
+	SignUp(ctx *gin.Context)
+	SignIn(ctx *gin.Context)
+}
+
+type authController struct {
+	authService services.AuthService
+}
+
+func NewAuthController(authService services.AuthService) AuthController {
+	return &authController{authService}
+}
+
+func (authController *authController) SignUp(ctx *gin.Context) {
+	// NOTE: リクエストデータを構造体に変換
+	requestParams := dto.SignUpRequest{}
+	if err := ctx.ShouldBind(&requestParams); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": err,
+		})
+		return
+	}
+	result := authController.authService.SignUp(ctx, requestParams)
+
+	if result.Error == nil {
+		ctx.JSON(http.StatusOK, gin.H{
+			"user": result.User,
+		})
+		return
+	}
+
+	switch result.ErrorType {
+	case "internalServerError":
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": result.Error,
+		})
+	case "validationError":
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": utils.CoordinateValidationErrors(result.Error),
+		})
+	}
+}
+
+func (authController *authController) SignIn(ctx *gin.Context) {
+	requestParams := dto.SignInRequest{}
+	if err := ctx.ShouldBind(&requestParams); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": err,
+		})
+		return
+	}
+	result := authController.authService.SignIn(ctx, requestParams)
+
+	if result.NotFoundMessage != "" {
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"error": result.NotFoundMessage,
+		})
+		return
+	}
+	if result.Error != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": result.Error,
+		})
+		return
+	}
+
+	// NOTE: Cookieにtokenをセット
+	ctx.SetCookie("token", result.TokenString, 3600*24, "/", "localhost", false, true)
+	ctx.JSON(http.StatusOK, gin.H{
+		"token": result.TokenString,
+	})
+}
--- app/main.go
+++ app/main.go
@@ -1,49 +1,28 @@
 package main
 
 import (
-	models "app/models/generated"
-	"context"
-	"database/sql"
-	"fmt"
-	"log"
+	"app/controllers"
+	"app/db"
+	"app/routers"
+	"app/services"
 	"os"
 
+	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/volatiletech/sqlboiler/v4/boil"
-	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
-	dsn := os.Getenv("MYSQL_USER") +
-		":" + os.Getenv("MYSQL_PASS") +
-		"@tcp(" + os.Getenv("MYSQL_HOST") + ":" + os.Getenv("MYSQL_PORT") + ")/" +
-		os.Getenv("MYSQL_DBNAME") +
-		"?charset=utf8mb4&collation=utf8mb4_general_ci&parseTime=true&loc=Local"
+	dbCon := db.Init()
 
-	db, err := sql.Open("mysql", dsn)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	// service
+	authService := services.NewAuthService(dbCon)
 
-	defer db.Close()
+	// controller
+	authController := controllers.NewAuthController(authService)
+	authRouter := routers.NewAuthRouter(authController)
 
-	ctx := context.Background()
-
-	// NOTE: SQL文を標準出力に出す
-	boil.DebugMode = true
-
-	hash, err := bcrypt.GenerateFromPassword([]byte("password"), bcrypt.DefaultCost)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	user := &models.User{
-		Name:     "test_name_2",
-		Email:    "test_2@example.com",
-		Password: string(hash),
-	}
-	createErr := user.Insert(ctx, db, boil.Infer())
-	if createErr != nil {
-		log.Fatalln(createErr)
-	}
-	fmt.Println(user.Reload(ctx, db))
+	// router
+	r := gin.Default()
+	authRouter.SetRouting(r)
+	r.Run(":" + os.Getenv("SERVER_PORT"))
 }
