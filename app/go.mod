345d17f4d4b40137f449189818189ef8c5172757
yamao-sys <yamaopanku@gmail.com>
2024-10-26 14:31:40 +0900

    Userのデータ生成ができることを確認

--- app/go.mod
+++ app/go.mod
@@ -1,3 +1,20 @@
 module app
 
 go 1.23.1
+
+require (
+	github.com/friendsofgo/errors v0.9.2
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/volatiletech/null/v8 v8.1.2
+	github.com/volatiletech/sqlboiler/v4 v4.16.2
+	github.com/volatiletech/strmangle v0.0.7
+	golang.org/x/crypto v0.0.0-20220826181053-bd7e27e6170d
+)
+
+require (
+	github.com/gofrs/uuid v4.2.0+incompatible // indirect
+	github.com/spf13/cast v1.5.0 // indirect
+	github.com/volatiletech/inflect v0.0.1 // indirect
+	github.com/volatiletech/randomize v0.0.1 // indirect
+	golang.org/x/xerrors v0.0.0-20220609144429-65e65417b02f // indirect
+)
--- app/main.go
+++ app/main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	models "app/models/generated"
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/volatiletech/sqlboiler/v4/boil"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func main() {
-	fmt.Println("Hello Golang!")
+	dsn := os.Getenv("MYSQL_USER") +
+		":" + os.Getenv("MYSQL_PASS") +
+		"@tcp(" + os.Getenv("MYSQL_HOST") + ":" + os.Getenv("MYSQL_PORT") + ")/" +
+		os.Getenv("MYSQL_DBNAME") +
+		"?charset=utf8mb4&collation=utf8mb4_general_ci&parseTime=true&loc=Local"
+
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer db.Close()
+
+	ctx := context.Background()
+
+	// NOTE: SQL文を標準出力に出す
+	boil.DebugMode = true
+
+	hash, err := bcrypt.GenerateFromPassword([]byte("password"), bcrypt.DefaultCost)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	user := &models.User{
+		Name:     "test_name_2",
+		Email:    "test_2@example.com",
+		Password: string(hash),
+	}
+	createErr := user.Insert(ctx, db, boil.Infer())
+	if createErr != nil {
+		log.Fatalln(createErr)
+	}
+	fmt.Println(user.Reload(ctx, db))
 }
